6c1b8560fc7b434c5042007a28adfc79443154a6
OlgaZhivaeva <olga.zhiv@yandex.ru>
2024-04-28 17:04:09 +0300

    View_added

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 
 
 def passcard_info_view(request, passcode):
-    passcard = Passcard.objects.get(id=passcode)
+    passcard = Passcard.objects.get(passcode=passcode)
     this_passcard_visits = []
     for visit in Visit.objects.filter(passcard=passcard):
         this_passcard_visit = {
@@ -13,8 +13,8 @@ def passcard_info_view(request, passcode):
                 'is_strange': visit.is_visit_long(),
             }
         this_passcard_visits.append(this_passcard_visit)
-        context = {
-            'passcard': passcard,
-            'this_passcard_visits': this_passcard_visits
-        }
-        return render(request, 'passcard_info.html', context)
+    context = {
+        'passcard': passcard,
+        'this_passcard_visits': this_passcard_visits
+    }
+    return render(request, 'passcard_info.html', context)
--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -10,9 +10,10 @@ def storage_information_view(request):
                 'who_entered': visit.passcard.owner_name,
                 'entered_at': visit.entered_at,
                 'duration': visit.get_duration(),
+                'is_strange': visit.is_visit_long(),
             }
         non_closed_visits.append(non_closed_visit)
-        context = {
-            'non_closed_visits': non_closed_visits,  # не закрытые посещения
-        }
-        return render(request, 'storage_information.html', context)
+    context = {
+        'non_closed_visits': non_closed_visits,  # не закрытые посещения
+    }
+    return render(request, 'storage_information.html', context)
